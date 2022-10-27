54f45edc3eac2e1c9ab177bfaeb07f636679d3b4
rajvinoth.codem <rajvinoth.swaminathan@codem.com>
2022-10-27 17:28:08 +0530

    removed unwanted css from header

--- src/inc/header.php
+++ src/inc/header.php
@@ -15,7 +15,6 @@ $users = new Users();
   <title>USER MANAGEMENT</title>
   <link rel="stylesheet" href="assets/bootstrap.min.css">
   <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
-  <link rel="stylesheet" href="assets/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
